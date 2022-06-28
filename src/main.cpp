047cbce8ebe27649aa05da5004d2e5769c1dca52
IceDefcon <ice,marek@yahoo.com>
2022-06-29 00:36:31 +0200

    Version 2.3 fixed

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,22 @@
-
-
-
-
 #include <iostream>     // system
 #include "dump.h"
 
 
-int a = 0xFFFFABCD;
-int b = 0x0000ABCD;
+int a = 0xFFFF1234;
+int b = 0x00001234;
 
 int main(int argc, char* argv[]) 
 {
     system("clear");
 
-    RegDump();
+    int * pa = &a;
+    int * pb = &b;
+
+    printf("poiter a ---> %x\n",pa);
+    printf("poiter b ---> %x\n",pb);
+
+    //RegDump();
 
     return 0;
 }
+

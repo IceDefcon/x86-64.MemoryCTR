9c39cc49d1caadd113c6c0f9b846e7c95bd3941d
IceDefcon <ice.defcon@yahoo.com>
2022-06-30 15:35:12 +0200

    Version 3.2 Function Pointers

--- include/mem.h
+++ include/mem.h
@@ -6,12 +6,21 @@
 extern "C" unsigned long _MEMORY_READ_A(void);
 extern "C" unsigned long _MEMORY_READ_B(void);
 
-int * MemDump(void)
-{ 
-    printf("RAX_Accumulator ---> Read A : %#018" PRIx64 " \n",_MEMORY_READ_A());
-    printf("RAX_Accumulator ---> Read B : %#018" PRIx64 " \n",_MEMORY_READ_B());
 
-    return 0;
-}
+class MemAssembly{
+
+    public:
+        MemAssembly();
+        ~MemAssembly();
+        
+        int AssemblyMemoryDump(void);
+
+        void MemDump(void)
+        { 
+            printf("RAX_Accumulator ---> Read A : %#018" PRIx64 " \n",_MEMORY_READ_A());
+            printf("RAX_Accumulator ---> Read B : %#018" PRIx64 " \n\n",_MEMORY_READ_B());
+        }
+
+};
 
 #endif /* MEM_H */
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,60 @@
 #include <iostream>     // system
 #include "mem.h"
 
-unsigned int a = 0xFFFF1234;
-unsigned int b = 0x00001234;
+using namespace std;
+
+#define MemAssemblyLength 8
+
+
+unsigned int A = 0xFFFF1234;
+unsigned int B = 0x00001234;
+
+void Function_A(void)
+{
+    printf("This is execution of the void Function A \n");
+}
+
+void Function_B(int x)
+{
+    x = x + 1;
+    printf("This is execution of the void Function B with variable x:%d \n",x);
+}
 
 int main(int argc, char* argv[]) 
 {
-
     system("clear");
 
-    unsigned int * pa = &a;
-    unsigned int * pb = &b;
+    unsigned int * pA = &A;
+    unsigned int * pB = &B;
 
-    printf("Original Data a ---> 0x%08x\n",a);
-    printf("Original Data b ---> 0x%08x\n\n",b);
+    printf("Original Data A ---> 0x%08x\n",A);
+    printf("Original Data B ---> 0x%08x\n\n",B);
 
-    printf("Linker Pointer pa ---> %p\n",pa);
-    printf("Linker Pointer pb ---> %p\n\n",pb);
+    printf("Pointer to A variable ---> %p\n",pA);
+    printf("Pointer to B variable ---> %p\n\n",pB);
 
-    MemDump();
+    void (*pFunction_A)(void) = &Function_A;
+    void (*pFunction_B)(int)  = &Function_B;
+
+    MemAssembly * pMemAssembly;
+    pMemAssembly->MemDump();
+
+    //
+    // Testing function pointers
+    //
+    pFunction_A();
+    pFunction_B(100);
+    (*pFunction_B)(200);
+
+    printf("Pointer to Function_A ---> %p\n",pFunction_A);
+    printf("Pointer to Function_B ---> %p\n\n",pFunction_B);
     
+    //
+    // Not Fixed Yet ---> Address is not linked
+    //
+    printf("Pointer to MemAssembly Class ---> %p\n\n",pMemAssembly);
+
+    //void (*pTest)(void) = &pMemAssembly->MemDump();
+
     return 0;
 }
